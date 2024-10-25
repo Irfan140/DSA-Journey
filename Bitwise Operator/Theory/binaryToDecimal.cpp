0dc97c703726cf28a2632d07408046204e56cded
Irfan140 <irfanmehmud140@gmail.com>
2024-10-25 15:44:14 +0530

    some other stuff studied

--- Bitwise Operator/Theory/binaryToDecimal.cpp
+++ Bitwise Operator/Theory/binaryToDecimal.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 int binary_to_decimal(string &binary){
-    int n=binary.size();
+    int n = binary.size();
     int result =0;
     for(int i=n-1;i>=0;i--){
-        char ch =binary[i];
-        int num= ch-'0';
-        result = result + num*(1<<(n-i-1));
+        char ch = binary[i];
+        int num = ch-'0';
+        result += num*(1<<(n-i-1));
     }
     return result;
 }
