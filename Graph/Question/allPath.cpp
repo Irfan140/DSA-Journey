b89242bb1f0b9cb3bcfdac95496f1b48a6c90942
Irfan140 <irfanmehmud140@gmail.com>
2024-10-29 10:45:03 +0530

    solved one question All path uisng DFS

--- Graph/Question/allPath.cpp
+++ Graph/Question/allPath.cpp
@@ -1,3 +1,5 @@
+//? Given a graph . Calculate all paths between two given nodes
+
 //? Given a graph check wheather there is a path between any two given vertices ?
 
 #include<iostream>
@@ -9,26 +11,36 @@ using namespace std;
 vector<list<int> > graph;
 int v; // no. of vertices
 unordered_set<int> visited;
+vector<vector<int> > result;
 
 void add_edge(int src, int dest, bool bi_dir = true){
     graph[src].push_back(dest);
     if(bi_dir) graph[dest].push_back(src);
 }
 
-bool dfs(int curr, int end){
-    if(curr==end) return true;
+void dfs(int curr, int end, vector<int> &path){
+    if(curr==end){
+        path.push_back(curr);
+        result.push_back(path);
+        path.pop_back();
+        return;
+    }
     visited.insert(curr); // marked visited
+    path.push_back(curr);
     for(auto neighbour : graph[curr]){
         if(!visited.count(neighbour)){
-            bool result = dfs(neighbour,end);
-            if(result) return true;
+            dfs(neighbour,end, path);
+            
         }
     }
-    return false;
+    path.pop_back();
+    visited.erase(curr);
+    return;
 }
 
-bool anyPath(int src, int dest){
-    return dfs(src,dest);
+void allPath(int src, int dest){
+    vector<int> v;
+     dfs(src,dest,v);
 }
 
 int main(){
@@ -44,9 +56,15 @@ int main(){
     }
     int x, y;
     cin>>x>>y;
-    cout<<anyPath(x,y)<<endl;
+    allPath(x,y);
+    for(auto path : result){
+        for(auto el : path){
+            cout<<el<<" ";
+        }
+        cout<<endl;
+    }
 }
 
-// for weighted give false to add_edge()
+
 
 // solved using DFS technique
